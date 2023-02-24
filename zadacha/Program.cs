c34fb47eccb0c55c18f06fc74628897bf0416fdf
rkvk <rukavichkina@tut.by>
2023-02-24 11:46:03 +0300

    zadacha

---
+++ zadacha/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] array1 = new string[5] {"the", "end", "hello", "world", "bye"};
+string[] array2 = new string[array1.Length];
+int count = 0;
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
